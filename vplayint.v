59943184fb09519f97e7b047d81faee5c78462bd
Linklancien <linklancien@gmail.com>
2025-06-14 15:43:57 +0200

    Update vplayint.v

--- vplayint.v
+++ vplayint.v
@@ -524,7 +524,7 @@ pub fn suppress_tabs(text string) []string {
 	mut final := []string{}
 	for phrase in tempo {
 		mut max_id := 0
-		for id in phrase.len {
+		for id in 0..phrase.len {
 			// 9 is the u8 corresponding to tabs
 			if phrase[id] == 9 {
 				max_id = id
